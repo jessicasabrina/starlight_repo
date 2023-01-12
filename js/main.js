896bab7413718aa0c3a585fa606a1067441e988b
Jessica Fernandez <jessica.sabrina.fernandez@gmail.com>
2023-01-12 18:46:33 -0300

    javascript segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,154 +1,265 @@
-// EJEMPLO DESAFÍO ENTREGABLE
+// Array para el carrito de compras
+const carrito = []
 
-const comprarProductos = () => {
-    let producto = '';
-    let cantidad = 0;
-    let precio = 0;
-    let totalCompra = 0;
-    let seguirComprando = false;
+// Ordenar productos de menor a mayor
+const ordenarMenorMayor = () => {
+    productos.sort((a, b) => a.precio - b.precio)
+    mostrarListaOrdenada()
+};
+
+// Ordenar productos de mayor a menor
+const ordenarMayorMenor = () => {
+    productos.sort((a, b) => b.precio - a.precio)
+    mostrarListaOrdenada()
+};
+
+const mostrarListaOrdenada = () => {
+    const listaDeProductos = productos.map(producto => {
+        return '- '+producto.nombre+' $'+producto.precio
+    })
+    alert('Lista de precios:'+'\n\n'+listaDeProductos.join('\n'))
+    comprarProductos(listaDeProductos)
+};
+
+const comprarProductos = (listaDeProductos) => {
+    let productoNombre = ''
+    let productoCantidad = 0
+    let otroProducto = false
 
     do {
-        producto = prompt ("Selecciona el producto a comprar del 1 al 9");
-        cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
+        productoNombre = prompt('¿Qué producto desea comprar?'+'\n\n'+listaDeProductos.join('\n'))
+        productoCantidad = parseInt(prompt('¿Cuántos queres comprar?'))
 
-        let cantidadValidada = validarCantidad(cantidad);
+        const producto = productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
 
-        switch (producto) {
-            case "1":
-                precio = 600;
-                break;
-            case "2":
-                precio = 750;
-                break;
-            case "3":
-                precio = 500;
-                break;
-            case "4":
-                precio = 200;
-                break;
-            case "5":
-                precio = 1500;
-                break;
-            case "6":
-                precio = 500;
-                break;
-            case "7":
-                precio = 1000;
-                break;
-            case "8":
-                precio = 500;
-                break;
-            case "9":
-                precio = 100;
-                break;
-            default:
-                alert("Alguno de los datos ingresados no es correcto");
-                precio= 0;
-                cantidad= 0;
-        }
-
-        totalCompra += precio * cantidadValidada;
-        seguirComprando = confirm("¿Querés agregar otro producto?");
-
-    } while (seguirComprando){
-
-        const totalConDescuento = aplicarDescuento(totalCompra);
-        const totalConEnvio = calcularEnvio(totalConDescuento);
-    
-        return totalConEnvio;
-    }
-}
-
-const validarCantidad = (cantidad) => {
-    while (Number.isNaN(cantidad) || cantidad === 0) {
-        if (cantidad !== 0) {
-            alert('Deber agregar un número.')
+        if (producto) {
+            agregarAlCarrito(producto, producto.id, productoCantidad)
         } else {
-            alert('Debe agregar un número distinto de cero.')
+            alert('El producto no se encuentra en el catálogo!')
         }
-        cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
-    }
 
-    return cantidad;
+        otroProducto = confirm('Desea agregar otro producto?')
+    } while (otroProducto);
+
+    confirmarCompra()
 };
 
-const aplicarDescuento = (totalCompra) => {
-    let totalConDescuento = 0;
-
-    if (totalCompra >= 5000) {
-        totalConDescuento = totalCompra * 0.80;
-        return totalConDescuento;
+const agregarAlCarrito = (producto, productoId, productoCantidad) => {
+    const productoRepetido = carrito.find(producto => producto.id === productoId)
+    if (!productoRepetido) {
+        producto.cantidad += productoCantidad
+        carrito.push(producto)
     } else {
-        return totalCompra;
+        productoRepetido.cantidad += productoCantidad
     }
-}
-
-const calcularEnvio = (totalCompra) => {
-    let tieneEnvioADomicilio = false;
-
-    tieneEnvioADomicilio = confirm("¿Querés envío a domicilio?");
-
-    if (tieneEnvioADomicilio && totalCompra >= 2000) {
-      alert("Tenés envio gratis. El total de tu compra es $" + totalCompra);
-    } else if (tieneEnvioADomicilio && totalCompra < 2000 && totalCompra !== 0) {
-      totalCompra += 700;
-      alert("El envío cuesta $700. El total de tu compra es $" + totalCompra);
-    } else {
-      alert("El total de tu compra es $" + totalCompra);
-    }
-
-    return totalCompra;
-}
-
-function calcularCantidadDeCuotas() {
-    let cuotas = 0;
-    let tieneCuotas = false;
-
-    tieneCuotas = confirm("¿Querés pagar en cuotas?");
-
-    if(tieneCuotas) {
-        cuotas = parseInt(prompt("¿En cuántas cuotas querés pagar?"));
-        if (cuotas === 0){
-            cuotas = 1;
-        }else if (Number.isNaN(cuotas)){
-            calcularCantidadDeCuotas();
-        }
-    }else {
-        cuotas = 1;
-    }
-
-    return cuotas;
 };
 
-function calcularIntereses (cuotas) {
-    let tasa = 12.3;
-    let sinIntereses = 0;
-    let tasaTotal = 0;
-    let interesesTotales = 0;
+const eliminarProductoCarrito = (nombreProductoAEliminar) => {
+    carrito.forEach((producto, index) => {
+        if (producto.nombre.toLowerCase() === nombreProductoAEliminar.toLowerCase()) {
+            if (producto.cantidad > 1) {
+                producto.cantidad--
+            } else {
+                carrito.splice(index, 1)
+            }
+        }
+    })
+    confirmarCompra()
+};
 
-    if (cuotas === 1){
-        return sinIntereses;
-    }else{
-        tasaTotal = tasa + cuotas * 0.2;
-        interesesTotales = tasaTotal * cuotas;
-        return interesesTotales;
+const confirmarCompra = () => {
+    const listaProductos = carrito.map(producto => {
+        return '- '+producto.nombre+' | Cantidad: '+producto.cantidad
+    })
+
+    const isCheckout = confirm('Checkout: '
+        +'\n\n'+listaProductos.join('\n')
+        +'\n\nPara continuar presione "Aceptar" sino "Cancelar" para eliminar un producto del carrito'
+    )
+
+    if (isCheckout) {
+        finalizarCompra(listaProductos)
+    } else {
+        const nombreProductoAEliminar = prompt('Ingrese el nombre del producto a eliminar:')
+        eliminarProductoCarrito(nombreProductoAEliminar)
     }
-}
+};
 
-function calcularTotalAPagar (totalCompra, cuotas, intereses) {
-    totalCompra = (totalCompra + intereses)
-    let valorCuota = totalCompra / cuotas;
-    alert ("El total a pagar es $"+totalCompra+" en "+cuotas+" cuotas de $"+valorCuota.toFixed(2));
-}
+const finalizarCompra = (listaProductos) => {
+    const cantidadTotal = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+    const precioTotal = carrito.reduce((acc, item) => acc + (item.cantidad * item.precio), 0)
+    alert('Detalle de su compra: '
+        +'\n\n'+listaProductos.join('\n')
+        +'\n\nTotal de productos: '+cantidadTotal
+        +'\n\nEl total de su compra es: '+precioTotal
+        +'\n\nGracias por su compra!'
+    )
+};
 
-// Invocamos a la función comprarProductos() para obtener el precio total de la compra.
-function comprar() {
-    const totalCompra = comprarProductos();
-    // Calculamos la cantidad de cuotas.
-    const cuotas = calcularCantidadDeCuotas();
-    // Calculamos los intereses.
-    const intereses = calcularIntereses(cuotas);
+const comprar = () => {
+    const productosBaratos = confirm('¿Querés ordenar la lista de productos del mas barato al mas caro?')
 
-    // Calculamos el total a pagar y mostramos un mensaje al usuario.
-    calcularTotalAPagar(totalCompra, cuotas, intereses);
-}
+    if (productosBaratos) {
+        ordenarMenorMayor()
+    } else {
+        ordenarMayorMenor()
+    }
+};
+
+
+//comprar()
+
+// // EJEMPLO DESAFÍO ENTREGABLE
+
+// const comprarProductos = () => {
+//     let producto = '';
+//     let cantidad = 0;
+//     let precio = 0;
+//     let totalCompra = 0;
+//     let seguirComprando = false;
+
+//     do {
+//         producto = prompt ("Selecciona el producto a comprar del 1 al 9");
+//         cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
+
+//         let cantidadValidada = validarCantidad(cantidad);
+
+//         switch (producto) {
+//             case "1":
+//                 precio = 600;
+//                 break;
+//             case "2":
+//                 precio = 750;
+//                 break;
+//             case "3":
+//                 precio = 500;
+//                 break;
+//             case "4":
+//                 precio = 200;
+//                 break;
+//             case "5":
+//                 precio = 1500;
+//                 break;
+//             case "6":
+//                 precio = 500;
+//                 break;
+//             case "7":
+//                 precio = 1000;
+//                 break;
+//             case "8":
+//                 precio = 500;
+//                 break;
+//             case "9":
+//                 precio = 100;
+//                 break;
+//             default:
+//                 alert("Alguno de los datos ingresados no es correcto");
+//                 precio= 0;
+//                 cantidad= 0;
+//         }
+
+//         totalCompra += precio * cantidadValidada;
+//         seguirComprando = confirm("¿Querés agregar otro producto?");
+
+//     } while (seguirComprando){
+
+//         const totalConDescuento = aplicarDescuento(totalCompra);
+//         const totalConEnvio = calcularEnvio(totalConDescuento);
+    
+//         return totalConEnvio;
+//     }
+// }
+
+// const validarCantidad = (cantidad) => {
+//     while (Number.isNaN(cantidad) || cantidad === 0) {
+//         if (cantidad !== 0) {
+//             alert('Deber agregar un número.')
+//         } else {
+//             alert('Debe agregar un número distinto de cero.')
+//         }
+//         cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
+//     }
+
+//     return cantidad;
+// };
+
+// const aplicarDescuento = (totalCompra) => {
+//     let totalConDescuento = 0;
+
+//     if (totalCompra >= 5000) {
+//         totalConDescuento = totalCompra * 0.80;
+//         return totalConDescuento;
+//     } else {
+//         return totalCompra;
+//     }
+// }
+
+// const calcularEnvio = (totalCompra) => {
+//     let tieneEnvioADomicilio = false;
+
+//     tieneEnvioADomicilio = confirm("¿Querés envío a domicilio?");
+
+//     if (tieneEnvioADomicilio && totalCompra >= 2000) {
+//       alert("Tenés envio gratis. El total de tu compra es $" + totalCompra);
+//     } else if (tieneEnvioADomicilio && totalCompra < 2000 && totalCompra !== 0) {
+//       totalCompra += 700;
+//       alert("El envío cuesta $700. El total de tu compra es $" + totalCompra);
+//     } else {
+//       alert("El total de tu compra es $" + totalCompra);
+//     }
+
+//     return totalCompra;
+// }
+
+// function calcularCantidadDeCuotas() {
+//     let cuotas = 0;
+//     let tieneCuotas = false;
+
+//     tieneCuotas = confirm("¿Querés pagar en cuotas?");
+
+//     if(tieneCuotas) {
+//         cuotas = parseInt(prompt("¿En cuántas cuotas querés pagar?"));
+//         if (cuotas === 0){
+//             cuotas = 1;
+//         }else if (Number.isNaN(cuotas)){
+//             calcularCantidadDeCuotas();
+//         }
+//     }else {
+//         cuotas = 1;
+//     }
+
+//     return cuotas;
+// };
+
+// function calcularIntereses (cuotas) {
+//     let tasa = 12.3;
+//     let sinIntereses = 0;
+//     let tasaTotal = 0;
+//     let interesesTotales = 0;
+
+//     if (cuotas === 1){
+//         return sinIntereses;
+//     }else{
+//         tasaTotal = tasa + cuotas * 0.2;
+//         interesesTotales = tasaTotal * cuotas;
+//         return interesesTotales;
+//     }
+// }
+
+// function calcularTotalAPagar (totalCompra, cuotas, intereses) {
+//     totalCompra = (totalCompra + intereses)
+//     let valorCuota = totalCompra / cuotas;
+//     alert ("El total a pagar es $"+totalCompra+" en "+cuotas+" cuotas de $"+valorCuota.toFixed(2));
+// }
+
+// // Invocamos a la función comprarProductos() para obtener el precio total de la compra.
+// function comprar() {
+//     const totalCompra = comprarProductos();
+//     // Calculamos la cantidad de cuotas.
+//     const cuotas = calcularCantidadDeCuotas();
+//     // Calculamos los intereses.
+//     const intereses = calcularIntereses(cuotas);
+
+//     // Calculamos el total a pagar y mostramos un mensaje al usuario.
+//     calcularTotalAPagar(totalCompra, cuotas, intereses);
+// }
---
+++ js/stock.js
@@ -0,0 +1,83 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Pelcuhe",
+        tipo: "peluche",
+        descripcion: "peluche pulpo rosa",
+        precio: 2200,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Oreo Rosa",
+        tipo: "caja",
+        descripcion: "caja en forma de oreo",
+        precio: 6500,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Joystick",
+        tipo: "periferico",
+        descripcion: "joystick xbox x/s",
+        precio: 30000,
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Keycap",
+        tipo: "keycap",
+        descripcion: "keycap umbreon",
+        precio: 6000,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Mouse Razer",
+        tipo: "mouse",
+        descripcion: "mouse razer viper ultimate",
+        precio: 8000,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "Humificador",
+        tipo: "humificador",
+        descripcion: "humificador rgb",
+        precio: 5000,
+        cantidad:0
+    },
+
+    {
+        id: 7,
+        nombre: "Almohada",
+        tipo: "almohada",
+        descripcion: "almohada dxracer",
+        precio: 6000,
+        cantidad:0
+    },
+
+    {
+        id: 8,
+        nombre: "Figura",
+        tipo: "figura",
+        descripcion: "figura vi league of legends",
+        precio: 5000,
+        cantidad:0
+    },
+
+    {
+        id: 9,
+        nombre: "Figura gato bebe",
+        tipo: "figura",
+        descripcion: "figura de plástico de gato bebe",
+        precio: 1000,
+        cantidad:0
+    }
+]
